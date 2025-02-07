6113221c2bd3d83447dd7b834ce43d3533f29609
Angus Chum <52922624+anguschum99@users.noreply.github.com>
2025-02-07 14:41:36 -0500

    add Angular localization support and Bootstrap integration added modal support added modal methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Job } from './job';
 import { JobService } from './job.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-root',
@@ -22,6 +23,19 @@ export class AppComponent implements OnInit {
     this.getJobs();
   }
 
+  public onOpenModal(job: Job | null, mode: string): void {
+    const modalId = mode === 'add' ? 'addJobModal' : 
+                    mode === 'edit' ? 'editJobModal' : 
+                    'deleteJobModal';
+    
+    const modalElement = document.getElementById(modalId);
+    if (modalElement) {
+      const modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    }
+  }
+
+
   public getJobs(): void {
     this.jobService.getJobs().subscribe({
       next: (response: Job[]) => {
@@ -33,4 +47,18 @@ export class AppComponent implements OnInit {
       }
     });
   }
+
+  public addJob(job: Job): void {
+    this.jobService.addJob(job).subscribe({
+      next: (response: Job) => {
+        console.log(response);
+        this.getJobs();
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error(error);
+        alert(error.message);
+      }
+    });
+  }
+
 }
